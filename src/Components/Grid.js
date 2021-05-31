7c67f922678160725ed22380eec934a00e9f5376
Austin M. Edger <austinedger0811@gmail.com>
2021-05-31 01:46:57 -0400

    added grid start and end

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -7,13 +7,31 @@ const useStyles = makeStyles({
         display: 'grid',
         gridTemplateColumns: `repeat(${props.colums}, 1fr)`, 
         gridTemplateRows: `repeat(${props.rows}, 1fr)`,
-        gridGap: '0',
         alignSelf: 'flex-start',
         width: props.colums * 20,
         height: props.rows * 20,
-        justifyContent: 'center'
+        justifyContent: 'center',
     }),
   });
+
+function initGrid(rows, colums) {
+    var grid = []
+    for (let row = 0; row < rows; row ++) {
+        grid.push([])
+        for (let col = 0; col < colums; col ++) {
+            grid[row].push(<Node key={`${col}${row}`} color={'white'} />)
+        }
+    }
+    return grid;
+}
+
+function setStart(grid, row, col) {
+    grid[row][col] = <Node key={`${col}${row}`} color={'green'} /> 
+}
+
+function setEnd(grid, row, col) {
+    grid[row][col] = <Node key={`${col}${row}`} color={'red'} /> 
+}
 
 function Grid(props) {
 
@@ -21,14 +39,10 @@ function Grid(props) {
 
     const { rows, colums } = props;
 
-    // initalize grid
-    var grid = []
-    for (let row = 0; row < rows; row ++) {
-        grid.push([])
-        for (let col = 0; col < colums; col ++) {
-            grid[row].push(<Node key={`${col}${row}`} />)
-        }
-    }
+    var grid = initGrid(rows, colums);
+    setStart(grid, 4, 4);
+    setEnd(grid, 35, 35);
+
 
     return (
         <div className={classes.root}>
--- src/Components/Node.js
+++ src/Components/Node.js
@@ -2,16 +2,17 @@ import React from 'react'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 
 const useStyles = makeStyles({
-    root: {
+    root: props => ({
         width: 20,
         height: 20,
         border: '1px solid black',
-    },
+        backgroundColor: props.color
+    }),
   });
 
 function Node(props) {
 
-    let classes = useStyles();
+    let classes = useStyles(props);
 
     return (
         <div className={classes.root} />
